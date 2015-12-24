48789d3ebad8491550aceeb4d8a8a33c7543f67d
Phoenix Chu <phoenixchu.com@gmail.com>
2015-12-24 15:09:50 -0800

    reverse twiter geo coordinates because it is using [lat,lon] format, but elastic serach is using GeoJson which is [lon, lat] format

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,6 +4,7 @@ class Tweet
   def initialize(attributes={})
     @attributes = attributes
     @attributes[:created_at] = @attributes[:created_at].to_datetime
+    @attributes[:geo][:coordinates].reverse! if @attributes[:geo] && @attributes[:geo][:coordinates]
   end
 
   def to_hash
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,19 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, :type => :model do
+
   it "should convert string to datetime" do
     tweet = Tweet.new(
       "id_str": '679936067110334468',
       "created_at": "Thu Dec 24 05:55:23 +0000 2015",
       "text": 'hello twitter',
-      "coordinates": [
-        50,
-        50
-      ],
+      "geo": {
+        "coordinates": [
+          50,
+          50
+        ]
+      },
       "entities": {
         "hashtags": ["twitter"]
       }
     )
     expect(tweet.to_hash[:created_at]).to be_a DateTime
+  end
+
+  it "should reverse order of twitter geo coordinates. come on! twiiter, why would you do that!" do
+    tweet = Tweet.new(
+      "id_str": '679936067110334468',
+      "created_at": "Thu Dec 24 05:55:23 +0000 2015",
+      "text": 'hello twitter',
+      "geo": {
+        "coordinates": [
+          50,
+          139
+        ]
+      },
+      "entities": {
+        "hashtags": ["twitter"]
+      }
+    )
+    expect(tweet.to_hash[:geo][:coordinates][0]).to eq 139
+    expect(tweet.to_hash[:geo][:coordinates][1]).to eq 50
   end
 end
