231f111414f0b27825c795809c93cdfc368d3e28
Phoenix Chu <phoenixchu.com@gmail.com>
2015-12-25 15:27:40 -0800

    fixed an empty hashtag bug

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,10 +2,10 @@ class TweetsController < ApplicationController
 
   def index
     repo = TweetRepository.new
-    lon = params[:lon] || 122.2361
+    lon = params[:lon] || -122.2361
     lat = params[:lat] || 37.4828
     radius = params[:radius] || 3
-    hashtag = params[:hashtag]
+    hashtag = params[:hashtag].present? ? params[:hashtag] : nil
     lon = lon.to_f
     lat = lat.to_f
     radius = radius.to_i
